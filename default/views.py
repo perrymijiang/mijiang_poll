271057660ded0855ddd8a99438df05c5ba2a29eb
米漿 <perrywu0814@gmail.com>
2019-10-31 10:49:24 +0800

    分開的頁面範本

--- default/views.py
+++ default/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.views.generic import ListView, DetailView
+from .models import *
 
 # Create your views here.
+def poll_list(req):
+    polls = poll.objects.all()
+    return render_to_response('poll_list.html', {'polls': polls})
+
+
+class PollList(ListView):
+    model = poll
+
+class PollDetail(DetailView):
+    model = poll
