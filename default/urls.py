50d44b3889de6831c16f497e1a75233edd7d6fa4
米漿 <perrywu0814@gmail.com>
2019-11-07 12:06:56 +0800

    新增投票主題

--- default/urls.py
+++ default/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('poll/', views.PollList.as_view()),
-    path('poll/<int:pk/', views.PollDetail.as_view)
+    path('poll/<int:pk>/', views.PollDetail.as_view()),
+    path('poll/vote/<int:oid>/', views.PollVote.as_view()),
+    path('create/' , PollCreate.as_view()),
+    path('<int:pk>/update/', PollUpdate.as_view()),
 ]
--- default/views.py
+++ default/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, render_to_response
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, RedirectView,CreateView, UpdateView
 from .models import *
 
 # Create your views here.
@@ -13,3 +13,25 @@ class PollList(ListView):
 
 class PollDetail(DetailView):
     model = poll
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['options'] = Option.objects.filter(poll_id=self.kwargs['pk'])
+        return ctx
+
+class PollVote(RedirectView):
+    def get_redirect_url(self, **kwargs):
+        opt = Option.objects.get(id=self.kwargs['oid'])
+        opt.count += 1
+        opt.save()
+        return "/poll/{}/".format(opt.poll_id)
+
+class PollCreate(CreateView):
+    model = Poll
+    fields = ['subject']
+    success_url = "/poll/"
+
+class PollUpdate(UpdateView):
+    model = Poll
+    field = ['subject']
+    success_url = "/poll/"
